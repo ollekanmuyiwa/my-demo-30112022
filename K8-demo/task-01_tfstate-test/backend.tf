c97d4180258f9a2c2f1ea3920c15cd34c9e62768
Amir Aliyu <hello-reussi@outlook.com>
2022-12-01 08:19:06 +0000

    third commit

--- K8-demo/task-01_tfstate-test/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "tfstate"
-        storage_account_name = "<storage_acct_name>"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
-}
